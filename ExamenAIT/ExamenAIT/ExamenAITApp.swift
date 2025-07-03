4ba057b5a50c3af2d9671be5c3eb7f65685f8afe
José Antonio Campos <fred-mercury@hotmail.com>
2025-07-03 08:23:27 -0600

    Primer commit

---
+++ ExamenAIT/ExamenAIT/ExamenAITApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenAITApp.swift
+//  ExamenAIT
+//
+//  Created by Antonio Bedolla on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ExamenAITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
